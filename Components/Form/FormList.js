9094fcb62b5246240649937ecf3278c186478e37
Corentin Faget <faget.corentin@gmail.com>
2021-04-05 16:20:57 +0200

    Ajout d'un bouton d'information sur la page des formulaires

--- Components/Form/FormList.js
+++ Components/Form/FormList.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View } from 'react-native';
-import { StyleSheet, FlatList } from 'react-native'
+import { View, StyleSheet, FlatList, TouchableOpacity, Image, Modal, Text } from 'react-native'
 import forms from '../../Helpers/formData'
 import FormListItem from './FormListItem';
 
 class FormList extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            modalVisible: false
+        }
+    }
+
+    setInfoPopUpVisibility(){
+        this.setState({ modalVisible: !this.state.modalVisible });
+    }
+
     displayFormDescription = (form) => {
         this.props.navigation.navigate("Description", { form: form });
     }
@@ -28,6 +38,27 @@ class FormList extends React.Component{
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({item}) => <FormListItem form={item} getColors={this.getColors} displayFormDescription={this.displayFormDescription}/>}
                 />
+                <View style={styles.button_container}>
+                    <TouchableOpacity style={styles.button_info} onPress={() => this.setInfoPopUpVisibility()}>
+                        <Image
+                            source={require('../../Images/info.png')}
+                            style={styles.icon}
+                        />
+                    </TouchableOpacity>     
+                </View>
+                <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={this.state.modalVisible}>
+                    <View style={styles.modal_view}>
+                        <Text style={styles.modal_title}>Informations</Text>
+                        <Text style={styles.modal_text}>Allow this app to access motion sensors? </Text>
+                        <TouchableOpacity style={styles.button} onPress={() => this.setInfoPopUpVisibility()}>
+                            <Text style={styles.button_text}>OK</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Modal>
+                
             </View>
         );
     }
@@ -37,6 +68,66 @@ const styles = StyleSheet.create({
     main_container: {
         flex: 1,
         backgroundColor: '#441d59',
+    },
+    button_container: {
+        position: 'absolute',
+        height: '100%',
+        marginLeft: 10
+    },
+    button_info: {
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 100,
+        marginBottom: 10,
+        marginTop: 'auto'
+    },
+    icon: {
+        width: 30,
+        height: 30,
+        resizeMode: 'contain',
+    },
+    
+    modal_view: {
+        margin: 20,
+        height: '95%',
+        backgroundColor: "#241332",
+        borderTopRightRadius:60,
+        borderBottomLeftRadius:60,
+        padding: 25,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0,
+        shadowRadius: 4,
+        elevation: 5,
+        
+    },
+    modal_text: {
+        marginTop: 10,
+        color: '#ffffff',
+        textAlign: "center"
+    },
+    modal_title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#ffffff',
+        textAlign: "center"
+    },
+    button: {
+        padding: 10,
+        backgroundColor: '#862db3',
+        borderRadius: 20,
+        alignItems: 'center',
+        alignSelf: 'center',
+        width: '60%',
+        marginTop: 'auto',
+        marginBottom: 20
+    },
+    button_text: {
+        color: '#ffffff'
     }
 });
 
